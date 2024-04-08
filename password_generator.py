087320dd6fde181ae7e640fe94f202e472711512
DianaZhu6 <44934152+DianaZhu6@users.noreply.github.com>
2024-04-08 19:13:52 -0400

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -3,7 +3,68 @@
 class PasswordGenerator:
   def generate_password(self):
     """
-    generate passwords
+    Generates a secure password
+    Args: 
+      None
+    Returns:
+      secure password (string)
     """
-  #other methods
+  def save_password_to_file(self, password, filename):
+    """
+    Creates a file to save the password to
+    Args:
+      password (string) - the user inputed password
+      filename (string) - the name of the file to be created
+    Returns:
+      file with password
+    """
+  def check_password_strength(self, password):
+    """
+    Ensures that the password is strong
+    Args: 
+      password (string) - the user's password
+    Returns:
+      True/False (boolean) - returns True if the password is strong or False if it is not 
+    """
+    pass
+
+  def generate_passphrase_based_on_word(self, word):
+    """
+    Creates a passphrase (string of words) based on a word given by the user
+    Args:
+      word (string) - the user's inputed key word
+    Returns:
+      passphrase (string)
+    """
+    pass
+
+  def character_substitution(self, password):
+    """
+    Substitute characters in the given password
+    Args:
+      password (string) - the user's given password
+    Returns:
+      character substituted password (string)
+    """
+    pass
+
+  def encrypt_password(self, password):
+    """
+    Encrypts the password with MD5 
+    Args:
+      password (string) - the user's given password
+    Returns:
+      encrypted password (string)
+    """
+    pass
+
+  def decrypt_password(self, passwordEncrypted):
+    """
+    Decrypts password (assuming it was encrypted with MD5)
+    Args:
+      passwordEncrypted (string) - encrypted password
+    Returns:
+      decrypted password (string)
+    """
+    pass 
 
