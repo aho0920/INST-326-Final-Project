5bf805b24c8dbff4e65c0392cac79a07e3766520
DianaZhu6 <44934152+DianaZhu6@users.noreply.github.com>
2024-04-08 13:19:26 -0400

    Update password_generator_gui.py

--- password_generator_gui.py
+++ password_generator_gui.py
@@ -1,1 +1,19 @@
+import tkinter as tk
+from tkinter import ttk
+from password_generator import PasswordGenerator
 
+class PasswordGeneratorGUI:
+  def __init__(self, root):
+    """
+    creating the initial gui 
+    """
+
+  def generate_password(self):
+    """
+    call generate password 
+    """
+
+def create_gui():
+  root = tk.Tk()
+  app = PasswordGeneratorGUI(root)
+  root.mainloop()
