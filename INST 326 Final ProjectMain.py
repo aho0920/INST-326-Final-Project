ebf0c90e16d69df288428727b3c2ec14fa24d589
DianaZhu6 <44934152+DianaZhu6@users.noreply.github.com>
2024-04-08 19:14:50 -0400

    Update INST 326 Final ProjectMain.py

--- INST 326 Final ProjectMain.py
+++ INST 326 Final ProjectMain.py
@@ -1,2 +1,7 @@
 # INST 326 Final Project
 # Andrew Ho, Diana Zhu, Emani Gray, Makachi Ngobili
+
+from password_generator_gui import create_gui
+
+if __name__ == "__main__":
+    create_gui() #start the gui 
