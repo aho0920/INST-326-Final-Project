ec8e4fa4a2f0d7c0896b24873b70444879b65d8e
DianaZhu6 <44934152+DianaZhu6@users.noreply.github.com>
2024-04-08 17:53:26 -0400

    Update password_generator_gui.py

--- password_generator_gui.py
+++ password_generator_gui.py
@@ -5,15 +5,30 @@ from password_generator import PasswordGenerator
 class PasswordGeneratorGUI:
   def __init__(self, root):
     """
-    creating the initial gui 
+    Creates the initial GUI 
+    Args:
+      root (Tk) - the main window all items will be in
+    Returns:
+      None
     """
 
   def generate_password(self):
     """
-    call generate password 
+    Generates a random password using the PasswordGenerator class
+    Args:
+        self (PasswordGeneratorGUI): The current instance of the PasswordGeneratorGUI class.
+    Returns:
+        None  
     """
 
 def create_gui():
+  """
+  Initializes the GUI window
+  Args: 
+    None
+  Returns:
+    None
+  """
   root = tk.Tk()
   app = PasswordGeneratorGUI(root)
   root.mainloop()
